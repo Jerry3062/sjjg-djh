991548cfb96ee061889eb65fe01fc19bd41e688a
Jerry3062 <qwe258sj@163.com>
2018-07-03 09:57:00 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_336ef.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_336ef.dir/feature_tests.cxx.obj"
+  "cmTC_336ef.pdb"
+  "cmTC_336ef.exe"
+  "cmTC_336ef.exe.manifest"
+  "libcmTC_336ef.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_336ef.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
